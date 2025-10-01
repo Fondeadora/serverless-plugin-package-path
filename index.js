3d91da68808a42d4603da92842cbd414c4b1dcc9
daninfpj <daninfpj@gmail.com>
2025-10-01 12:35:29 -0600

    fix: packaging

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const fs = require("fs").promises;
+const fs = require("fs");
+const fsPromises = require("fs").promises;
 const JSZip = require("jszip");
 const path = require("path");
 
@@ -10,125 +11,93 @@ class ServerlessPluginPackagePath {
     this.options = options;
     this.provider = this.serverless.getProvider("aws");
 
-    this.commands = {};
-
     this.hooks = {
-      "before:package:finalize": this.repackageLayer.bind(this),
+      "after:package:createDeploymentArtifacts":
+        this.updateServicePath.bind(this),
+      "before:package:compileLayers": this.updateRequirementsPath.bind(this),
     };
   }
 
-  async repackageLayer() {
-    this.serverless.cli.log("Repackaging layers with custom path...");
+  async updateServicePath() {
+    this.serverless.cli.log("Updating service files paths...");
 
-    const layers = Object.keys(this.serverless.service.layers || {});
+    const layers = this.serverless.service.layers;
+    const packagesPath = this.serverless.service.custom.packagePath.path;
 
-    // If no layers defined (using pythonRequirements.layer), check for pythonRequirements.zip
-    if (layers.length === 0) {
-      const serverlessDir = path.join(
-        this.serverless.config.servicePath,
-        ".serverless"
-      );
-      const pythonReqZip = path.join(serverlessDir, "pythonRequirements.zip");
-
-      if (
-        await fs
-          .access(pythonReqZip)
-          .then(() => true)
-          .catch(() => false)
-      ) {
-        return this._repackageSingleLayer(pythonReqZip);
-      } else {
-        this.serverless.cli.log("No layers found to repackage");
-        return;
-      }
-    }
-
-    return await Promise.all(
-      layers.map(async (layerName) => {
+    await Promise.all(
+      Object.keys(layers).map(async (layerName) => {
         const layerObject = this.serverless.service.getLayer(layerName);
-        const artifactFilePath = this._artifactFilePath(layerName);
-        return this._repackageSingleLayer(artifactFilePath, layerObject.path);
+        const artifactFilePath = this._getArtifactPath(layerName);
+
+        const artifactBuffer = await fsPromises.readFile(artifactFilePath);
+
+        const tmpPackage = new JSZip();
+        const serviceFolder = tmpPackage.folder(
+          path.join(packagesPath, layerObject.path)
+        );
+        await serviceFolder.loadAsync(artifactBuffer);
+
+        await Promise.all([
+          this._writeZipToFile(new JSZip(), artifactFilePath),
+          this._writeZipToFile(tmpPackage, this._getTmpPath()),
+        ]);
       })
     );
   }
 
-  async _repackageSingleLayer(artifactFilePath, layerPath = "") {
-    const targetPath = this.serverless.service.custom.packagePath.path;
+  async updateRequirementsPath() {
+    this.serverless.cli.log("Updating requirements paths...");
 
-    // Check if the file is a symlink - if so, replace it with a real file
-    try {
-      const stats = await fs.lstat(artifactFilePath);
-      if (stats.isSymbolicLink()) {
-        const realPath = await fs.realpath(artifactFilePath);
-        // Read from the cached file
-        const cachedBuffer = await fs.readFile(realPath);
-        // Remove the symlink
-        await fs.unlink(artifactFilePath);
-        // Write the cached content as a real file
-        await fs.writeFile(artifactFilePath, cachedBuffer);
-      }
-    } catch (e) {
-      // File might not exist or other error, continue anyway
-    }
+    const layers = this.serverless.service.layers;
+    const packagesPath = this.serverless.service.custom.packagePath.path;
+    const tmpFilePath = this._getTmpPath();
 
-    // Read the original zip once
-    const originalBuffer = await fs.readFile(artifactFilePath);
-    const originalZip = await JSZip.loadAsync(originalBuffer);
+    await Promise.all(
+      Object.keys(layers).map(async (layerName) => {
+        const artifactFilePath = this._getArtifactPath(layerName);
 
-    // Create new zip with restructured paths
-    const newZip = new JSZip();
-    const targetFolder = path.join(targetPath, layerPath);
+        const [artifactBuffer, tmpBuffer] = await Promise.all([
+          fsPromises.readFile(artifactFilePath),
+          fsPromises.readFile(tmpFilePath),
+        ]);
 
-    // Move all files to the new path structure
-    const filePromises = [];
-    originalZip.forEach((relativePath, file) => {
-      if (!file.dir) {
-        // Strip 'python/' prefix if it exists (serverless-python-requirements adds it)
-        const cleanPath = relativePath.startsWith("python/")
-          ? relativePath.substring("python/".length)
-          : relativePath;
+        const artifact = new JSZip();
+        await artifact.loadAsync(tmpBuffer);
 
-        const newPath = path.join(targetFolder, cleanPath);
-        filePromises.push(
-          file.async("nodebuffer").then((content) => {
-            newZip.file(newPath, content, {
-              binary: true,
-              compression: "DEFLATE",
-            });
-          })
-        );
-      }
-    });
+        const packagesFolder = artifact.folder(packagesPath);
+        await packagesFolder.loadAsync(artifactBuffer);
 
-    await Promise.all(filePromises);
-
-    // Write the new zip back to the artifact file
-    return this._writeZipToFile(newZip, artifactFilePath);
-  }
-
-  _artifactFilePath(layerName) {
-    const zipFileName = `${layerName}.zip`;
-
-    return path.join(
-      this.serverless.config.servicePath,
-      ".serverless",
-      zipFileName
+        await this._writeZipToFile(artifact, artifactFilePath);
+      })
     );
   }
 
-  async _writeZipToFile(zipFile, filePath) {
-    const fsSync = require("fs");
+  _getArtifactPath(layerName) {
+    return path.join(
+      this.serverless.config.servicePath,
+      ".serverless",
+      `${layerName}.zip`
+    );
+  }
+
+  _getTmpPath() {
+    return path.join(
+      this.serverless.config.servicePath,
+      ".serverless",
+      "tmp.zip"
+    );
+  }
+
+  _writeZipToFile(zipFile, filePath) {
     return new Promise((resolve, reject) => {
       zipFile
         .generateNodeStream({
           type: "nodebuffer",
           streamFiles: true,
           compression: "DEFLATE",
-          compressionOptions: {
-            level: 9,
-          },
+          compressionOptions: { level: 9 },
         })
-        .pipe(fsSync.createWriteStream(filePath))
+        .pipe(fs.createWriteStream(filePath))
         .on("finish", resolve)
         .on("error", reject);
     });
